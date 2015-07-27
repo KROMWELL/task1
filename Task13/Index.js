0ece7f36b3fed38cc7265e1f0cf462b7fc4703f3
unknown <nos12feratu@gmail.com>
2015-07-27 06:17:04 +0300

    add Task13

---
+++ Task13/Index.js
@@ -0,0 +1,54 @@
+
+
+var express = require("express");
+var bodyParser = require("body-parser");
+//var socketio = require("socket.io");
+
+
+var app = express();
+
+var server = app.listen(8080);
+//var io = socket.listen(server);
+
+
+var staticDir = __dirname + "/public/";
+app.use(bodyParser.urlencoded({extended: false}));
+
+var messages = [];
+
+app.get("/", function(req, res){
+	res.sendFile(staticDir + "Index.html");
+});
+
+/*app.get("/sockets", function(req, res){
+	res.sendFile(staticDir + "index_io.html");
+});*/
+
+
+app.get("/messages", function(req, res){
+	res.json(messages);
+});
+
+app.post("/messages", function(req, res){
+	var message = req.body;
+	messages.push(message);
+	res.json(message);
+})
+
+/*io.on("connection", function(socket){
+	
+	console.log("Client connected");
+	
+	socket.on("disconnected", function(msg){
+		console.log("Client disconnected");
+	});
+	
+	socket.on("chat message", function(msg){
+		mesages.push(msg);
+		io.emit("chat message", msg);
+	});
+	
+	socket.emit("chat history", messages);
+	
+	
+})*/
